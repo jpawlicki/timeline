d80cd12e8f236be7a219e13d9dc612c80c9b086f
Joshua Pawlicki <gilgarn@gmail.com>
2016-01-07 08:04:10 -0800

    Voronoi, continents.

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
 import java.awt.*;
 import java.awt.geom.Path2D;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 	public static TriangleTree tree;
@@ -38,13 +44,61 @@ public class Main {
 		}*/
 
 		firstUpdate();
+/*
+ * For every triangle, generate one coordinate (the midpoint), attach it to all three vertexes.
+ * For every vertex, sort the coordinates in clockwise order and call that a polygon.
+ */
+
+		HashMap<Vertex, ArrayList<Vertex>> vCoords = new HashMap<>(); 
+		HashMap<Vertex, HashSet<Vertex>> adjacents = new HashMap<>(); 
+		for (TriangleTree t : tree.leafs.leafs) {
+			Vertex x = new Vertex(MathManager.normalize(t.triangle.circumcircle.circumcenter));
+			for (Vertex v : t.triangle.verts) {
+				// Add x to v's corners.
+				ArrayList<Vertex> vc = vCoords.get(v);
+				if (vc == null) {
+					vc = new ArrayList<Vertex>();
+					vCoords.put(v, vc);
+				}
+				vc.add(x);
+				// Track the adjacent vertexes of v as well.
+				HashSet<Vertex> adj = adjacents.get(v);
+				if (adj == null) {
+					adj = new HashSet<Vertex>();
+					adjacents.put(v, adj);
+				}
+				for (Vertex vv : t.triangle.verts) {
+					if (vv != v) {
+						adj.add(vv);
+					}
+				}
+			}
+		}
+
+		HashMap<Vertex, Region> regionMap = new HashMap<>();
+		ArrayList<Region> regions = new ArrayList<>();
+
+		for (Map.Entry<Vertex, ArrayList<Vertex>> e : vCoords.entrySet()) {
+			Region r = new Region(e.getKey(), e.getValue().toArray(new Vertex[0]));
+			regions.add(r);
+			regionMap.put(e.getKey(), r);
+		}
+
+		for (Map.Entry<Vertex, Region> e : regionMap.entrySet()) {
+			for (Vertex a : adjacents.get(e.getKey())) {
+				e.getValue().addAdjacent(regionMap.get(a));
+			}
+		}
+
+		setBiomes(regions);
+
 		while(true) {
 			update();
 			Graphics2D g = (Graphics2D)c.getBufferStrategy().getDrawGraphics();
 			g.setPaint(new Color(0, 0, 0));
 			g.fillRect(0, 0, 900, 900);
 			camera += 0.005;
-			draw(g, tree);
+			draw(g, regions);
 			g.dispose();
 			c.getBufferStrategy().show();
 			try {
@@ -54,23 +108,22 @@ public class Main {
 		}
 	}
 
-	public static void draw(Graphics2D g, TriangleTree root) {
+	public static void draw(Graphics2D g, ArrayList<Region> regions) {
 		double viewTheta = 1 / 2.0;
 		double[][] rotz = MathManager.fetchrot3d(new double[]{0, 0, 1}, camera);
 		double[] viewVector = MathManager.matmult(rotz, new double[]{1, 0, 0});
-		for (TriangleTree t : root.leafs.leafs) {
-			double[][] pp = new double[3][];
+		for (Region r : regions) {
+			double[][] pp = new double[r.corners.length][];
 			boolean draw = false;
-			for (int i = 0; i < t.triangle.verts.length; i++) {
-				pp[i] = t.triangle.verts[i].point;
+			for (int i = 0; i < r.corners.length; i++) {
+				pp[i] = r.corners[i].point;
 				if (MathManager.dot3d(pp[i], viewVector) > viewTheta) {
 					draw = true;
 				}
 			}
 			if (!draw) continue;
 			pp = MathManager.matmult(pp, rotz);
-			float[] color = Triangle.getColor(t.triangle.color);
-			g.setPaint(new Color(color[0], color[1], color[2]));
+			g.setPaint(r.color);
 			drawPolySimple(pp, g);
 		}
 	}
@@ -95,8 +148,8 @@ public class Main {
 						MathManager.scale3d(b, i / (numPoints - 1.0))));
 			}
 			for (int i = 1; i < numPoints; i++) {
-				x2 = (int)(width / 2 + points[i][1] * width / (2 - points[i][0]));
-				y2 = (int)(height + points[i][2] * width / (2 - points[i][0]));
+				x2 = (int)(width / 2 + points[i][1] * width / (3 - points[i][0]));
+				y2 = (int)(height / 2 + points[i][2] * width / (3 - points[i][0]));
 				if (first) {
 					first = false;
 					path.moveTo(x1, y1);
@@ -112,9 +165,10 @@ public class Main {
 
 	public static void drawPolySimple(double[][] poly, Graphics2D g) {
 		Path2D.Double path = new Path2D.Double();
-		path.moveTo(width / 2 + poly[0][1] * width / (2 - poly[0][0]), height + poly[0][2] * width / (2 - poly[0][0]));
-		path.lineTo(width / 2 + poly[1][1] * width / (2 - poly[1][0]), height + poly[1][2] * width / (2 - poly[1][0]));
-		path.lineTo(width / 2 + poly[2][1] * width / (2 - poly[2][0]), height + poly[2][2] * width / (2 - poly[2][0]));
+		path.moveTo(width / 2 + poly[0][1] * width / (2.5 - poly[0][0]), height / 2 + poly[0][2] * width / (2.5 - poly[0][0]));
+		for (int i = 1; i < poly.length; i++) {
+			path.lineTo(width / 2 + poly[i][1] * width / (2.5 - poly[i][0]), height / 2 + poly[i][2] * width / (2.5 - poly[i][0]));
+		}
 		path.closePath();
 		g.fill(path);
 	}
@@ -139,6 +193,116 @@ public class Main {
 			tree.add(new Vertex(add));
 		}
 	}
+
+	private static void setBiomes(ArrayList<Region> regions) {
+		ArrayList<HashSet<Region>> continents = new ArrayList<>();
+		ArrayList<LinkedList<Region>> continentAdjacents = new ArrayList<>();
+		HashSet<Region> unmarked = new HashSet<>(regions);
+		for (int i = 0; i < 32; i++) { // 32 continental plates.
+			Region r = rand(regions);
+			while (!unmarked.contains(r)) {
+				r = rand(regions);
+			}
+			unmarked.remove(r);
+			HashSet<Region> continent = new HashSet<>();
+			continent.add(r);
+			continents.add(continent);
+			LinkedList<Region> l = new LinkedList<>();
+			for (Region a : r.adjacents) {
+				l.add(a);
+			}
+			continentAdjacents.add(l);
+		}
+		while (!unmarked.isEmpty()) {
+			System.out.println(unmarked.size());
+			int c = (int)(Math.random() * continents.size());
+			LinkedList<Region> candidates = continentAdjacents.get(c);
+			if (candidates.isEmpty()) {
+				continue;
+			}
+			Region r = candidates.remove((int)(Math.random() * candidates.size()));
+			if (!unmarked.contains(r)) {
+				continue;
+			}
+			unmarked.remove(r);
+			continents.get(c).add(r);
+			for (Region a : r.adjacents) {
+				candidates.add(a);
+			}
+		}
+
+		// Set the colors.
+		for (HashSet<Region> c : continents) {
+			boolean oceanic = Math.random() < 0.7;
+			Color color;
+			if (oceanic) {
+				color = new Color((float)Math.random()/10, (float)Math.random()/10, (float)Math.random()/2+0.4f);
+			} else {
+				color = new Color((float)Math.random()/10, (float)Math.random()/2+0.5f, (float)Math.random()/10);
+			}
+			for (Region r : c) {
+				r.color = color;
+			}
+		}
+	}
+
+	private static Region rand(List<Region> regions) {
+		return regions.get((int)(Math.random() * regions.size()));
+	}
+}
+
+class Region {
+	public Color color;
+	public Vertex[] corners;
+	public ArrayList<Region> adjacents = new ArrayList<>();
+
+	public Region(final Vertex origin, final Vertex... corners) {
+		color = new Color((float)Math.random(), (float)Math.random(), (float)Math.random());
+		final double[] op = origin.point;
+		final double[] zero = MathManager.normalize(MathManager.subtract3d(corners[0].point, MathManager.scale3d(op, MathManager.dot3d(op, corners[0].point))));
+		Arrays.sort(corners, new Comparator<Vertex>() {
+			@Override
+			public int compare(Vertex a, Vertex b) {
+				double[] ap = a.point;
+				// Project every vertex onto the plane normal to origin; normalize the projections.
+				ap = MathManager.normalize(MathManager.subtract3d(ap, MathManager.scale3d(op, MathManager.dot3d(op, ap))));
+				double adot = MathManager.dot3d(zero, ap);
+				double across = MathManager.dot3d(op, MathManager.cross3d(zero, ap));
+				double aval = 0;
+				if (across >= 0 && adot >= 0) {
+					aval = across;
+				} else if (across >= 0 && adot < 0) {
+					aval = 1 + (1 - across);
+				} else if (across <= 0 && adot < 0) {
+					aval = 2 - across;
+				} else {
+					aval = 4 + across;
+				}
+
+				double[] bp = b.point;
+				bp = MathManager.normalize(MathManager.subtract3d(bp, MathManager.scale3d(op, MathManager.dot3d(op, bp))));
+				double bdot = MathManager.dot3d(zero, bp);
+				double bcross = MathManager.dot3d(op, MathManager.cross3d(zero, bp));
+				double bval = 0;
+				if (bcross >= 0 && bdot >= 0) {
+					bval = bcross;
+				} else if (bcross >= 0 && bdot < 0) {
+					bval = 1 + (1 - bcross);
+				} else if (bcross <= 0 && bdot < 0) {
+					bval = 2 - bcross;
+				} else {
+					bval = 4 + bcross;
+				}
+
+				return (int)Math.signum(aval - bval);
+			}
+		});
+		this.corners = corners;
+	}
+
+	public void addAdjacent(Region r) {
+		adjacents.add(r);
+	}
 }
 
 class Vertex {
@@ -153,6 +317,27 @@ class Vertex {
 		point = MathManager.normalize(new double[] {r * Math.cos(theta), r * Math.sin(theta), u});
 	}
 }
+/**
+class VertexPair {
+	public final Vertex a;
+	public final Vertex b;
+	public VertexPair(Vertex a, Vertex b) {
+		this.a = a;
+		this.b = b;
+	}
+	public boolean equals(Object other) {
+		if (!other instanceof VertexPair) {
+			return false;
+		} else 
+			return (a == other.a && b == other.b) || (a == other.b && b == other.a);
+		} else {
+			return false;
+		}
+	}
+	public long hashCode() {
+		return a.hashCode() ^ b.hashCode();
+	}
+}*/
 
 class Sphere {
 	public final double radius;
@@ -345,6 +530,11 @@ class TriangleTree {
 	public void add(Vertex v) {
 		if (children.size() == 0) {
 			leafs.remove(this);
+			v = new Vertex(MathManager.normalize(
+					MathManager.scale3d(
+					MathManager.add3d(v.point,
+							MathManager.add3d(triangle.verts[0].point,
+									MathManager.add3d(triangle.verts[1].point, triangle.verts[2].point))), 3)));
 			TriangleTree a = new TriangleTree(triangle.verts[0], triangle.verts[1], v);
 			TriangleTree b = new TriangleTree(triangle.verts[1], triangle.verts[2], v);
 			TriangleTree c = new TriangleTree(triangle.verts[2], triangle.verts[0], v);
